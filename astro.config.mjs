56cf89945698bd54f1d3b46465371cb7b90ff737
marcebusi <marcebusignani@gmail.com>
2024-03-06 15:09:44 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+// https://astro.build/config                 
+ export default defineConfig({                 
+    integrations: [tailwind()],
+    i18n: {
+      defaultLocale: 'es',
+      locales: ['es', 'en'],
+      routing: {
+         prefixDefaultLocale: false
+      }
+    }
+ });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+    es: 'Español',
+    en: 'English',
+  };
+  
+  export const defaultLang = 'es';
+  
+  export const ui = {
+    es: {
+      'nav.home':'Inicio',
+      'nav.projects':'Proyectos',
+      'nav.about':'Sobre mí',
+      'nav.contact':'Contacto'
+    },
+    en: {
+        'nav.home':'Home',
+        'nav.projects':'Projects',
+        'nav.about':'About me',
+        'nav.contact':'Contact'
+    },
+  } as const;
